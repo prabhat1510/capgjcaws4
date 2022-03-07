3d8f3e313f1fcc289d3fbda59b94461739961399
prabhat1510 <prabhatc@gmail.com>
2022-03-07 13:31:14 +0530

    Spring Data JPA example updated

--- springmvcdatajpaapp/src/main/java/com/springmvcdatajpaapp/controller/EmployeeController.java
+++ springmvcdatajpaapp/src/main/java/com/springmvcdatajpaapp/controller/EmployeeController.java
@@ -5,13 +5,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springmvcdatajpaapp.model.Employee;
@@ -27,21 +26,44 @@ public class EmployeeController {
 		public ModelAndView getAllEmployees(){
 			List<Employee> listOfEmployee= employeeService.getAllEmployees();
 			Map employeeModel = new HashMap();
+			/**
+			Model model = null;
+			model.addAttribute("listOfEmployee", listOfEmployee);
+			return new ModelAndView("allemployee",model.asMap());
+			**/
 			if(!listOfEmployee.isEmpty()) {
 				employeeModel.put("listOfEmployee", listOfEmployee);
-				return new ModelAndView("allemployee",employeeModel);
+				return new ModelAndView("allemployee",employeeModel,HttpStatus.OK);
 			}else {
 				employeeModel.put("message","No employees found");
-				return new ModelAndView("nodatafound",employeeModel);
+				return new ModelAndView("nodatafound",employeeModel,HttpStatus.NOT_FOUND);
 			}
 					
 		}
-		/**
-		//Create
-		@PostMapping("/addemployee")
-		public ModelAndView addEmployee(Employee employee){
+		
+		//GetEmployeeForm 
+		@GetMapping("/employee")
+		public ModelAndView getEmployeeForm(@ModelAttribute Employee employee) {
+			ModelAndView modelAndView = new ModelAndView("employeeform");
 			return modelAndView;
-		}**/
+		}
+
+		//Create
+		@PostMapping("/saveemployee")
+		public ModelAndView addEmployee(Employee employee){
+			Employee emp = employeeService.addEmployee(employee);
+			ModelAndView modelAndView;
+			// Model in Spring Web MVC is a map
+			Map messageModel = new HashMap();
+			if (emp != null) {
+				messageModel.put("emplData", emp);
+				modelAndView = new ModelAndView("successWithData", messageModel);
+			} else {
+				messageModel.put("errormsg", "Unable to add employee");
+				modelAndView = new ModelAndView("errorMsg", messageModel);
+			}
+			return modelAndView;
+		}
 		//Retrieve
 		@GetMapping("/employeeById/{empId}")
 		public ModelAndView getEmployeeById(@PathVariable Integer empId){
